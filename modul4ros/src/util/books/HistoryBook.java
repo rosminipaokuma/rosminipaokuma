625558089831eaa895301c03de4059ade0f35c20
rosminipaokuma <108163610+rosminipaokuma@users.noreply.github.com>
2024-10-25 02:05:06 +0700

    Delete modul4ros directory

--- modul4ros/src/util/books/HistoryBook.java
+++
@@ -1,7 +0,0 @@
-package books;
-
-public class HistoryBook extends Book{
-    public HistoryBook(String idBuku, String judul, int stok, String category, String author) {
-        super(idBuku, judul, stok, category, author);
-    }
-}
